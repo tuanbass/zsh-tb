bc4e1c9af89e8a539a8b5a145f06c5610a7c7394
Tuan Bui <tuanba1@gmail.com>
2024-04-11 12:59:24 +0700

    add git plugin

---
+++ git.zsh
@@ -0,0 +1,11 @@
+if ((!$ + commands[git])); then
+	return
+fi
+
+alias ggeturl='git remote get-url'
+alias glog='git log --graph --pretty="format:%C(auto,yellow)%h%C(auto,magenta)% G? %C(auto,blue)%>(18,trunc)%ad %C(auto,green)%<(12,trunc)%aN%C(auto,reset)%s%C(auto,red)% gD% D"'
+alias gdh='git diff HEAD'
+alias gcld='git clone --depth 1'
+alias ghash='git rev-parse --short'
+alias ghashl='git rev-parse'
+alias tiga='tig --all'
