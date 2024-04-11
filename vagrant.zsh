81f60b3562801d30d29c095985bf07dd1d92dfe3
Tuan Bui <tuanba1@gmail.com>
2024-04-11 13:11:11 +0700

    add vagrant plugin

---
+++ vagrant.zsh
@@ -0,0 +1,11 @@
+if ((!$ + commands[vagrant])); then
+	return
+fi
+
+alias vgu='vagrant up'
+alias vgd='vagrant halt'
+alias vgdd='vagrant destroy'
+alias vggs='vagrant global-status'
+alias vgst='vagrant status'
+alias vgssh='vagrant ssh '
+alias vgrl='vagrant reload '
