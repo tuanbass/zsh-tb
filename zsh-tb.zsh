c2be56371beafcc1d699f48fd40fc9dd572409c9
Tuan Bui <tuanba1@gmail.com>
2024-05-24 11:36:20 +0700

    add a setup for yarn autocomplete

--- zsh-tb.zsh
+++ zsh-tb.zsh
@@ -1,3 +1,13 @@
 # function defaultAction() {
 #   echo "default action"
 # }
+
+
+# Dirty install for yarn autocomplete
+# if it was not setup, set it up
+
+YARN_AUTO_COMP_DIR=$HOME/.antidote/https-COLON--SLASH--SLASH-github.com-SLASH-g-plane-SLASH-zsh-yarn-autocompletions
+if [[ ! -f "$YARN_AUTO_COMP_DIR/yarn-autocompletions" ]]; then
+	cd $YARN_AUTO_COMP_DIR && ./zplug.zsh
+	echo "hello"
+fi
